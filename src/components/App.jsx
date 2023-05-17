dd80fbe3e702fff3ffb1aa0b558ab53411412038
OlegBilas <olegbilas@gmail.com>
2023-05-17 10:33:44 +0300

    refactoring from class to functional components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,59 @@
+import React, { useState } from 'react';
+
+import { Section } from 'components/Section/Section';
+import { FeedbackOptions } from 'components/FeedbackOptions/FeedbackOptions';
+import { Statistics } from 'components/Statistics/Statistics';
+import { Notification } from 'components/Notification/Notification';
+
 export const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const onLeaveFeedback = event => {
+    const nameButton = event.target.textContent;
+    if (nameButton === 'good') {
+      setGood(prevState => prevState + 1);
+    }
+    if (nameButton === 'neutral') {
+      setNeutral(prevState => prevState + 1);
+    }
+    if (nameButton === 'bad') {
+      setBad(prevState => prevState + 1);
+    }
+  };
+
+  const countTotalFeedback = () => {
+    return good + neutral + bad;
+  };
+
+  const countPositiveFeedbackPercentage = () => {
+    return Math.round((good / countTotalFeedback()) * 100);
+  };
+
+  const total = countTotalFeedback();
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Section title="Please leave feedback">
+        <FeedbackOptions
+          options={['good', 'neutral', 'bad']}
+          onLeaveFeedback={onLeaveFeedback}
+        />
+      </Section>
+      <Section title="Statistics">
+        {total ? (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={total}
+            positivePercentage={countPositiveFeedbackPercentage()}
+          />
+        ) : (
+          <Notification message="There is no feedback" />
+        )}
+      </Section>
+    </>
   );
 };
---
+++ src/components/Statistics/Statistics.styled.jsx
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const Facts = styled.p`
+  margin-bottom: 6px;
+  font-size: 18px;
+`;
